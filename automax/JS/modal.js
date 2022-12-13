192a520330f03c539a29ea86a186cafa00a6f1c0
Str0ng3r <maksstankevich81@gmail.com>
2022-12-13 23:40:30 +0300

    UPDATE

--- automax/JS/modal.js
+++ automax/JS/modal.js
@@ -12,3 +12,108 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+  
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-hodowa]"),
+      closeModalBtn: document.querySelector("[data-modal-close-hodowa]"),
+      modal: document.querySelector("[data-modal-hodowa]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-hamulce]"),
+      closeModalBtn: document.querySelector("[data-modal-close-hamulce]"),
+      modal: document.querySelector("[data-modal-hamulce]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-przenoszenie]"),
+      closeModalBtn: document.querySelector("[data-modal-close-przenoszenie]"),
+      modal: document.querySelector("[data-modal-przenoszenie]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-komputerowa]"),
+      closeModalBtn: document.querySelector("[data-modal-close-komputerowa]"),
+      modal: document.querySelector("[data-modal-komputerowa]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-spawal]"),
+      closeModalBtn: document.querySelector("[data-modal-close-spawal"),
+      modal: document.querySelector("[data-modal-spawal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-kosmetyka]"),
+      closeModalBtn: document.querySelector("[data-modal-close-kosmetyka]"),
+      modal: document.querySelector("[data-modal-kosmetyka]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-lakier]"),
+      closeModalBtn: document.querySelector("[data-modal-close-lakier]"),
+      modal: document.querySelector("[data-modal-lakier]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
